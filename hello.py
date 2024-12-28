c0f23b6c2d0e2c69faf9f4060cc395cd447392dd
Jun Guo <gjuoun@gmail.com>
2024-12-28 02:18:20 -0500

    feat: Implement transaction and block structure in blockchain system

--- hello.py
+++ hello.py
@@ -1,43 +1,59 @@
 import hashlib
 import time
-from typing import Any
+from typing import Any, List
+
+class Input:
+    def __init__(self, tx_id: str, output_index: int):
+        self.tx_id = tx_id
+        self.output_index = output_index
+
+class Output:
+    def __init__(self, value: float, recipient: str):
+        self.value = value
+        self.recipient = recipient
+
+class Transaction:
+    def __init__(self, version: int, inputs: List[Input], outputs: List[Output], locktime: int):
+        self.version = version
+        self.inputs = inputs
+        self.outputs = outputs
+        self.locktime = locktime
 
 class Block:
-    def __init__(self, index: int, previous_hash: str, timestamp: int, data: Any, hash: str):
+    def __init__(self, index: int, previous_hash: str, timestamp: int, data: List[Transaction]):
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = timestamp
         self.data = data
-        self.hash = hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        value = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}".encode()
+        return hashlib.sha256(value).hexdigest()
 
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
-        return Block(0, "0", int(time.time()), "Genesis Block", "0")
+        return Block(0, "0", int(time.time()), [])
 
     def get_latest_block(self):
         return self.chain[-1]
 
-    def add_block(self, data):
+    def add_block(self, transactions: List[Transaction]):
         latest_block = self.get_latest_block()
         new_index = latest_block.index + 1
         new_timestamp = int(time.time())
-        new_hash = self.calculate_hash(new_index, latest_block.hash, new_timestamp, data)
-        new_block = Block(new_index, latest_block.hash, new_timestamp, data, new_hash)
+        new_block = Block(new_index, latest_block.hash, new_timestamp, transactions)
         self.chain.append(new_block)
-
-    def calculate_hash(self, index, previous_hash, timestamp, data):
-        value = f"{index}{previous_hash}{timestamp}{data}".encode()
-        return hashlib.sha256(value).hexdigest()
 
     def is_chain_valid(self):
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i - 1]
 
-            if current_block.hash != self.calculate_hash(current_block.index, current_block.previous_hash, current_block.timestamp, current_block.data):
+            if current_block.hash != current_block.calculate_hash():
                 return False
 
             if current_block.previous_hash != previous_block.hash:
@@ -47,8 +63,10 @@ class Blockchain:
 
 def main():
     blockchain = Blockchain()
-    blockchain.add_block("First Block after Genesis")
-    blockchain.add_block("Second Block after Genesis")
+    transactions1 = [Transaction(1, [Input("tx_id_1", 0)], [Output(10.0, "recipient_1")], 0)]
+    transactions2 = [Transaction(1, [Input("tx_id_2", 0)], [Output(15.0, "recipient_2")], 0)]
+    blockchain.add_block(transactions1)
+    blockchain.add_block(transactions2)
 
     for block in blockchain.chain:
         print(f"Index: {block.index}, Data: {block.data}, Hash: {block.hash}")
