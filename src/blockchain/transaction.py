83fcad01cb59334afaec0b46a4aea54e2ca65f5d
Jun Guo <gjuoun@gmail.com>
2024-12-28 03:28:23 -0500

    feat: Add transaction module for blockchain functionality

---
+++ src/blockchain/transaction.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Input:
+    def __init__(self, tx_id: str, output_index: int):
+        self.tx_id = tx_id
+        self.output_index = output_index
+
+class Output:
+    def __init__(self, value: float, recipient: str):
+        self.value = value
+        self.recipient = recipient
+
+# remove the types from below. ai!
+class Transaction:
+    def __init__(self, version: int, inputs: List[Input], outputs: List[Output], locktime: int):
+        self.version = version
+        self.inputs = inputs
+        self.outputs = outputs
+        self.locktime = locktime
